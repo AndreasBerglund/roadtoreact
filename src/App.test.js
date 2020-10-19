d7366c5efeec50ad85b6c513081ebf60b12ebf87
andreasmonkey <andreas@brandmonkey.dk>
2020-10-19 21:01:45 +0200

    more testing

--- src/App.test.js
+++ src/App.test.js
@@ -117,4 +117,22 @@ test('renders the coorect lavbel', () => {
 
 })
   
+test('calls onSearchInput on input field change',  ()=> {
+  render(<SearchForm {...searchFormProps} />);
+  fireEvent.change(screen.getByDisplayValue('React'), {
+    target : { value : 'Redux'}
+  });
+
+  expect(searchFormProps.onSearchInput).toHaveBeenCalledTimes(1);
+
+});
+
+test('calls onSearchSubmit on button submit click',  ()=> {
+  render(<SearchForm {...searchFormProps} />);
+  fireEvent.submit(screen.getByRole('button'));
+
+  expect(searchFormProps.onSearchSubmit).toHaveBeenCalledTimes(1);
+
+});
+
 })
